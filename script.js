66a209d33050ab15a4b21a9ed66774d247b0eceb
Elronia <zar.catherine@gmail.com>
2021-03-08 23:54:01 -0500

    adding Event Listener for click event and creating function handleCheck

---
+++ script.js
@@ -0,0 +1,15 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e) {
+    //Check if they had the shift key down
+    // And check that they are checking it
+    if(e.shiftKey && this.checked) {
+        // go ahead and do what we please
+    }
+
+    lastChecked = this;
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
